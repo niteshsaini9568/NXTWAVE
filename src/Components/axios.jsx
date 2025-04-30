381875029db532ab8a42ecf0562dbe92f6d22b53
Nitesh saini <itsniteshsaini6376@gmail.com>
2025-04-30 10:24:38 +0530

    Fix backend

--- src/Components/axios.jsx
+++ src/Components/axios.jsx
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 export default axios.create({
-  // baseURL: 'https://nxtwave-l75r.onrender.com',
-  baseURL: 'http://localhost:8080',
+  baseURL: 'https://nxtwave-l75r.onrender.com',
+  // baseURL: 'http://localhost:8080',
   withCredentials: true,
 });
