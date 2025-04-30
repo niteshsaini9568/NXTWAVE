88b75bbf17c34ee65accc5efc0f00f8a2d678c75
Nitesh saini <itsniteshsaini6376@gmail.com>
2025-04-30 09:07:35 +0530

    Fixed bug

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,18 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import { resolve } from 'path';
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  resolve: {
+    alias: {
+      '@': resolve(process.cwd(), 'src'),
+    },
+  },
+  build: {
+    sourcemap: true,
+  },
   ssr: {
-    noExternal: ['react-router-dom']
-  }
-})
+    noExternal: ['react-router-dom', 'lucide-react'],
+  },
+});
